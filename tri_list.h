1b064cd5b6c065aa69d14c06bf67c6a971b43505
Paweł Balawender <pb429141@students.mimuw.edu.pl>
2022-01-28 02:55:59 +0100

    zrob wyciaganie odpowiedniego membera

--- tri_list.h
+++ tri_list.h
@@ -1,10 +1,16 @@
 #ifndef TRI_LIST_H
 #define TRI_LIST_H
 
+#include <numeric>
 #include <variant>
 #include <vector>
 
 #include "tri_list_concepts.h"
+
+
+// const correctness?
+// constexpr?
+
 
 // template <typename T, typename T1, typename T2, typename T3>
 // concept OneOf =
@@ -26,12 +32,27 @@ class tri_list : public std::ranges::view_interface<tri_list<T1, T2, T3>> {
   using elt_t = std::variant<T1, T2, T3>;
   std::vector<elt_t> buffer;
 
-  template<typename T>
-  std::function<T(T)> elt_modifier;
+  std::vector<std::function<T1(T1)>> modifiers_t1;
+  std::vector<std::function<T2(T2)>> modifiers_t2;
+  std::vector<std::function<T3(T3)>> modifiers_t3;
+
+  template <typename T>
+  std::vector<std::function<T(T)>>& get_modifiers() {
+    if constexpr (std::is_same<T, T1>::value) { return modifiers_t1; }
+    if constexpr (std::is_same<T, T2>::value) { return modifiers_t2; }
+    if constexpr (std::is_same<T, T3>::value) { return modifiers_t3; }
+  }
+
+  template <typename T>
+  T apply_modifiers(T t) {
+    auto modifiers = get_modifiers<T>();
+    return std::accumulate(modifiers.begin(), modifiers.end(), t, std::invoke);
+  }
 
   class iterator {
     using base_iterator = typename decltype(buffer)::iterator;
     base_iterator base;
+    tri_list* tri_lst;
 
   public:
     using iterator_category = typename base_iterator::iterator_category;
@@ -41,10 +62,10 @@ class tri_list : public std::ranges::view_interface<tri_list<T1, T2, T3>> {
     using reference = elt_t;
     
     iterator() = default;
-    explicit iterator(const base_iterator& other) : base(other) {}
+    explicit iterator(tri_list* tri_lst, const base_iterator& other)
+      : tri_lst(tri_lst), base(other) {}
 
-    // TUTAJ MUSI BYC EWALUACJA
-    reference operator*() const { return (*base); }
+    reference operator*() const { return tri_list::apply_modifiers(*base); }
     iterator& operator++() { base++; return *this; }
     iterator& operator--() { base--; return *this; }
     iterator operator++(int) { iterator result {*this}; ++this; return result; }
@@ -55,9 +76,6 @@ class tri_list : public std::ranges::view_interface<tri_list<T1, T2, T3>> {
 public:
   tri_list() : tri_list({}) { assert(false); }
   tri_list(std::initializer_list<elt_t> elements) : buffer(elements) {
-    elt_modifier<T1> = identity<T1>;
-    elt_modifier<T2> = identity<T2>;
-    elt_modifier<T3> = identity<T3>;
   };
   
   // template <OneOf<T1, T2, T3> T>
@@ -67,13 +85,13 @@ public:
   // template <OneOf<T1, T2, T3> T, modifier<T> F>
   template <typename T, modifier<T> F>
   void modify_only([[maybe_unused]] F m = F {}) {
-    elt_modifier<T> = compose<T>(m, elt_modifier<T>);
+    get_modifiers<T>().push_back(m);
   }
 
   // template <OneOf<T1, T2, T3> T>
   template <typename T>
   void reset() {
-      elt_modifier<T> = identity<T>;
+      get_modifiers<T>().clear();
   }
   
   // template <OneOf<T1, T2, T3> T>
@@ -81,11 +99,16 @@ public:
   auto range_over() {
     return buffer
       | std::ranges::views::filter(std::holds_alternative<T, T1, T2, T3>)
-      | std::ranges::views::transform([](std::variant<T1, T2, T3> x){ return std::get<T>(x);});
+      | std::ranges::views::transform(
+          [&](std::variant<T1, T2, T3> x){
+            return apply_modifiers(std::get<T>(x));
+          }
+        );
   }
 
-  iterator begin() { return iterator(buffer.begin()); };
-  iterator end() { return iterator(buffer.end()); }
+  iterator begin() { return iterator(this, buffer.begin()); };
+  iterator end() { return iterator(this, buffer.end()); }
 };
+
 
 #endif // TRI_LIST_H
